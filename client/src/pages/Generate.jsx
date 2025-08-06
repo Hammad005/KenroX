c48119bca0f5e59a2c7fe5406e4e821bdde1ff7d
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-08-06 18:20:25 +0500

    ✨ feat(generate): implement workout plan generation form
    - add stepper UI for multi-step form
    - implement input fields for age, height, weight, injuries
    - add checkbox components for workout days and fitness level
    - implement textarea input for fitness goals
    - include image and update form layout
    - add validation to ensure age is at least 13
    - display warning toast for invalid age input
    - add conditional rendering for submit button based on current step
    - update styling and layout for better responsiveness
    - add helper text for each input field
    - prevent user from going to the next step if the current step is not filled

--- client/src/pages/Generate.jsx
+++ client/src/pages/Generate.jsx
@@ -11,27 +11,28 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { ArrowLeft, ArrowRight, Power } from "lucide-react";
+import { ArrowLeft, ArrowRight, Check, Power } from "lucide-react";
 import React, { useRef, useState } from "react";
 import HeroWithBook from "../assets/Hero2.png";
+import { toast } from "sonner";
 
 const Generate = () => {
   const questions = [
-    { id: "Age", question: "What is your age?" },
-    { id: "Height", question: "What is your height(in ft)?" },
-    { id: "Weight", question: "What is your current body weight(in kg)?" },
-    { id: "Injuries", question: "Do you have any injuries or allergies?" },
+    { id: "age", question: "What is your age?" },
+    { id: "height", question: "What is your height(in ft)?" },
+    { id: "weight", question: "What is your current body weight(in kg)?" },
+    { id: "injuries", question: "Do you have any injuries or allergies?" },
     {
-      id: "Workout Days",
+      id: "workout_days",
       question: "Which days do you plan to work out each week?",
     },
     {
-      id: "Fitness Goals",
+      id: "fitness_goal",
       question:
         "What are your fitness goals - (such as weight loss, muscle gain, or endurance)?",
     },
     {
-      id: "Fitness Level",
+      id: "fitness_level",
       question:
         "What is your fitness level - (beginner, intermediate, or advanced)?",
     },
@@ -52,6 +53,8 @@ const Generate = () => {
     (q) => q.id === currentStep[currentStep.length - 1]
   );
 
+  const btnRef = useRef();
+
   const goNext = () => {
     if (currentIndex < questions.length - 1) {
       const nextId = questions[currentIndex + 1].id;
@@ -67,15 +70,38 @@ const Generate = () => {
     }
   };
 
-  const btnRef = useRef();
+  const checkDisabled = () => {
+    if (currentStep.includes("age") && !data.age) {
+      return true;
+    } else if (currentStep.includes("height") && !data.height) {
+      return true;
+    } else if (currentStep.includes("weight") && !data.weight) {
+      return true;
+    } else if (currentStep.includes("injuries") && !data.injuries) {
+      return true;
+    } else if (currentStep.includes("workout_days") && !data.workout_days) {
+      return true;
+    } else if (currentStep.includes("fitness_goal") && !data.fitness_goal) {
+      return true;
+    } else if (currentStep.includes("fitness_level") && !data.fitness_level) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (data.age < 13) {
+      setCurrentStep([questions[0].id]);
+      return toast.warning("You must be at least 13 years old.");
+    }
+
     const updatedData = {
       ...data,
-      height: data.height + " ft",
-      weight: data.weight + " kgs",
+      height: data.height + "ft",
+      weight: data.weight + "kgs",
     };
 
     setData(updatedData);
@@ -85,16 +111,8 @@ const Generate = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen md:px-22 px-4 md:pt-20 pt-10">
-      <div className="relative flex items-center justify-center w-full lg:min-h-screen lg:mt-20 ">
-        <div className="absolute -top-1/3 lg:translate-y-1/2 lg:block hidden">
-          <img
-            src={HeroWithBook}
-            alt="hero"
-            className="h-[300px] w-auto max-w-full object-contain"
-          />
-        </div>
-
-        <Card className="py-0 md:w-1/2 w-full border-primary-foreground/50 z-10 shadow shadow-primary-foreground">
+      <div className="flex items-center justify-center w-full">
+        <Card className="py-0 md:w-1/2 w-full border-primary-foreground/50 z-10 shadow shadow-primary-foreground gap-0">
           <CardHeader className="border-b bg-secondary rounded-t-xl py-4 px-4 [.border-b]:pb-2">
             <CardTitle className="font-sans flex items-center gap-2">
               <Power className="size-5 text-primary-foreground animate-pulse" />
@@ -110,7 +128,7 @@ const Generate = () => {
 
           <CardContent className="px-0">
             <div className="flex flex-col items-center justify-center">
-              <ul className="flex flex-wrap items-center justify-center w-full">
+              <ul className="flex flex-wrap items-center justify-center w-full my-5">
                 {questions.map((q, index) => {
                   const isVisited = currentStep.includes(q.id);
                   const isActive = q.id === currentStep[currentStep.length - 1];
@@ -126,7 +144,17 @@ const Generate = () => {
                             : "bg-secondary text-muted-foreground"
                         }`}
                       >
-                        {index + 1}
+                        {Array.isArray(data[q.id]) ? (
+                          data[q.id].length > 0 ? (
+                            <Check className="size-5" />
+                          ) : (
+                            index + 1
+                          )
+                        ) : data[q.id] ? (
+                          <Check className="size-5" />
+                        ) : (
+                          index + 1
+                        )}
                       </div>
 
                       {index !== questions.length - 1 && (
@@ -143,34 +171,47 @@ const Generate = () => {
                 })}
               </ul>
 
-              <div className="my-10 flex flex-col gap-3 items-start justify-start w-full px-6">
-                <Label
-                  htmlFor={questions[currentIndex].id}
-                  className={"font-sans"}
-                >
-                  {questions[currentIndex].question}
-                </Label>
-                <form onSubmit={handleSubmit} className="mt-2 w-full">
-                  {questions[currentIndex].id === "Age" && Age(setData, data)}
-                  {questions[currentIndex].id === "Height" &&
-                    Height(setData, data)}
-                  {questions[currentIndex].id === "Weight" &&
-                    Weight(setData, data)}
-                  {questions[currentIndex].id === "Injuries" &&
-                    Injuries(setData, data)}
-                  {questions[currentIndex].id === "Workout Days" &&
-                    WorkoutDays(setData, data)}
-                  {questions[currentIndex].id === "Fitness Goals" &&
-                    FitnessGoals(setData, data)}
-                  {questions[currentIndex].id === "Fitness Level" &&
-                    FitnessLevel(setData, data)}
+              <div className="flex w-full justify-between lg:items-center items-end">
+                {/* Left Image Section */}
+                <div className="flex h-[200px] w-1/3 items-end justify-center object-contain">
+                  <img
+                    src={HeroWithBook}
+                    alt="Hero"
+                    className="h-auto w-auto  object-cover"
+                  />
+                </div>
 
-                  <button type="submit" className="hidden" ref={btnRef} />
-                </form>
+                {/* Right Form Section */}
+                <div className="flex h-[200px] w-full flex-col items-start justify-center gap-3 px-2">
+                  <Label
+                    htmlFor={questions[currentIndex].id}
+                    className="font-sans text-xs md:text-sm"
+                  >
+                    {questions[currentIndex].question}
+                  </Label>
+
+                  <form onSubmit={handleSubmit} className="w-full">
+                    {questions[currentIndex].id === "age" && Age(setData, data)}
+                    {questions[currentIndex].id === "height" &&
+                      Height(setData, data)}
+                    {questions[currentIndex].id === "weight" &&
+                      Weight(setData, data)}
+                    {questions[currentIndex].id === "injuries" &&
+                      Injuries(setData, data)}
+                    {questions[currentIndex].id === "workout_days" &&
+                      WorkoutDays(setData, data)}
+                    {questions[currentIndex].id === "fitness_goal" &&
+                      FitnessGoals(setData, data)}
+                    {questions[currentIndex].id === "fitness_level" &&
+                      FitnessLevel(setData, data)}
+
+                    {/* Hidden button to allow form submit via ref */}
+                    <button type="submit" className="hidden" ref={btnRef} />
+                  </form>
+                </div>
               </div>
             </div>
           </CardContent>
-
           <CardFooter
             className={`border-t bg-secondary rounded-b-xl px-4 [.border-t]:pt-4 pb-4 gap-2 ${
               currentIndex === 0 ? "justify-end" : "justify-between"
@@ -183,9 +224,9 @@ const Generate = () => {
             >
               <ArrowLeft /> Back
             </Button>
-            <Button onClick={goNext}>
+            <Button disabled={checkDisabled()} onClick={goNext}>
               {currentIndex === questions.length - 1 ? (
-                "Submit"
+                "Generate"
               ) : (
                 <>
                   Next <ArrowRight />
@@ -203,44 +244,70 @@ export default Generate;
 
 export const Age = (setData, data) => {
   return (
-    <Input
-      type="number"
-      placeholder="Enter your age"
-      value={data.age}
-      onChange={(e) => setData({ ...data, age: e.target.value })}
-    />
+    <>
+      <Input
+        type="number"
+        placeholder="Enter your age"
+        value={data.age}
+        onChange={(e) =>
+          setData({
+            ...data,
+            age: e.target.value === "" ? null : Number(e.target.value),
+          })
+        }
+      />
+      <p className="text-[10px] text-muted-foreground pt-2 text-end w-full">
+        Age must be greater than or equal to 13.
+      </p>
+    </>
   );
 };
 
 export const Height = (setData, data) => {
   return (
-    <Input
-      type="number"
-      placeholder="Enter your height in ft"
-      value={data.height}
-      onChange={(e) => setData({ ...data, height: e.target.value })}
-    />
+    <>
+      <Input
+        type="text"
+        placeholder="Enter your height in ft"
+        value={data.height}
+        onChange={(e) => setData({ ...data, height: e.target.value })}
+      />
+      <p className="text-[10px] text-muted-foreground pt-2 text-end w-full">
+        Don't add "ft" or "feet" in your height field.
+      </p>
+    </>
   );
 };
 
 export const Weight = (setData, data) => {
   return (
-    <Input
-      type="number"
-      placeholder="Enter your weight in kgs"
-      value={data.weight}
-      onChange={(e) => setData({ ...data, weight: e.target.value })}
-    />
+    <>
+      <Input
+        type="text"
+        placeholder="Enter your weight in kgs"
+        value={data.weight}
+        onChange={(e) => setData({ ...data, weight: e.target.value })}
+      />
+      <p className="text-[10px] text-muted-foreground pt-2 text-end w-full">
+        Don't add "kg" or "kgs" in your weight field.
+      </p>
+    </>
   );
 };
 
 export const Injuries = (setData, data) => {
   return (
-    <Textarea
-      placeholder="Enter your injuries or allergies(if any)"
-      value={data.injuries}
-      onChange={(e) => setData({ ...data, injuries: e.target.value })}
-    />
+    <>
+      <Textarea
+        placeholder="Enter your injuries or allergies(if any)"
+        value={data.injuries}
+        onChange={(e) => setData({ ...data, injuries: e.target.value })}
+        className={"text-xs"}
+      />
+      <p className="text-[10px] text-muted-foreground pt-2 text-end w-full">
+        Enter your injuries or allergies(if any).
+      </p>
+    </>
   );
 };
 
@@ -255,7 +322,7 @@ export const WorkoutDays = (setData, data) => {
     "Sunday",
   ];
   return (
-    <div className="grid grid-cols-3 gap-3">
+    <div className="grid lg:grid-cols-3 grid-cols-2 gap-3">
       {days.map((day) => (
         <div key={day} className="flex items-center gap-2">
           <Checkbox
@@ -285,22 +352,46 @@ export const WorkoutDays = (setData, data) => {
 
 export const FitnessGoals = (setData, data) => {
   return (
-    <Input
-      type="text"
-      placeholder="Enter your fitness goals"
-      value={data.fitness_goal}
-      onChange={(e) => setData({ ...data, fitness_goal: e.target.value })}
-    />
+    <>
+      <Textarea
+        placeholder="Enter your fitness goal"
+        value={data.fitness_goal}
+        onChange={(e) => setData({ ...data, fitness_goal: e.target.value })}
+        className={"text-xs"}
+      />
+      <p className="text-[10px] text-muted-foreground pt-2 text-end w-full">
+        Fitness goal like (Lose Weight, Gain Strength, Improve Flexibility).
+      </p>
+    </>
   );
 };
 
 export const FitnessLevel = (setData, data) => {
+  const levels = ["Beginner", "Intermediate", "Advanced"];
   return (
-    <Input
-      type="text"
-      placeholder="Enter your fitness level"
-      value={data.fitness_level}
-      onChange={(e) => setData({ ...data, fitness_level: e.target.value })}
-    />
+    <>
+      <div className="grid gap-3">
+        {levels.map((level) => (
+          <div key={level} className="flex items-center gap-2">
+            <Checkbox
+              checked={data.fitness_level === level}
+              onCheckedChange={(checked) => {
+                if (checked) {
+                  setData({ ...data, fitness_level: level });
+                } else {
+                  setData({ ...data, fitness_level: "" });
+                }
+              }}
+            />
+            <span
+              key={level}
+              className="text-sm font-semibold text-muted-foreground"
+            >
+              {level}
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
